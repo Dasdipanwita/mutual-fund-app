ea2eccff31c4d97c3f4c5c51a3cc8ee00edeef7b
Dasdipanwita <162358570+Dasdipanwita@users.noreply.github.com>
2025-06-25 03:07:34 +0530

    Update authService.ts

--- frontend/src/services/authService.ts
+++ frontend/src/services/authService.ts
@@ -1,6 +1,3 @@
-// services/authService.ts
-import axios from 'axios';
-
 export const loginUser = async (email: string, password: string) => {
   const response = await axios.post(`${import.meta.env.VITE_API_URL}/api/auth/login`, {
     email,
