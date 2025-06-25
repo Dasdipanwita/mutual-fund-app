281a00d8fd049e9bc6aa5d66cf9cd26958399507
Dasdipanwita <162358570+Dasdipanwita@users.noreply.github.com>
2025-06-25 07:19:59 +0530

    Update server.mjs

--- backend/server.mjs
+++ backend/server.mjs
@@ -36,7 +36,7 @@ const PORT = process.env.PORT || 5000;
 
 // --- Middleware ---
 app.use(cors({
-  origin: ['https://mutualfund.netlify.app']', // Allow any origin
+  origin: ['https://mutualfund.netlify.app'], // Allow any origin
   credentials: true
 }));
 app.use(express.json({ limit: '50mb' }));
